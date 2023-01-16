653591877f964ca5fb915e14c7d5dd77a456c80e
Jérémy Martin <114619755+JeremyMrt@users.noreply.github.com>
2023-01-16 16:28:42 +0100

    Flow of form and board

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
 const startBtn = document.getElementById("start-btn");
 const entryForm = document.querySelector(".entry-form");
-const nameInput = document.getElementById("name").textContent;
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2");
+const markers = document.querySelectorAll(".marker");
+const nameInput = document.getElementById("name").value;
 const header = document.querySelector("header");
 const gameContent = document.querySelector(".game-content");
 
 startBtn.addEventListener("click", () => {
-  header.classList.add("hidden");
-  entryForm.classList.remove("hidden");
+  header.style.transform = "translateX(-100vw)";
+  header.style.transition = "transform 0.8s ease";
+
+  entryForm.style.transform = "translateX(0)";
+  entryForm.style.transition = "transform 0.8s ease";
+});
+
+function changeFormPage() {
+  form1.style.transform = "translateX(-100vw)";
+  form1.style.transition = "transform 0.8s ease";
+  form2.style.transform = "translateX(0)";
+  form2.style.transition = "transform 0.8s ease";
+}
+
+markers.forEach((marker) => {
+  marker.addEventListener("click", (e) => {
+    const chosenMarker = e.target.id;
+    const player1 = Player(nameInput, chosenMarker);
+
+    form2.style.transform = "translateX(-100vw)";
+    form2.style.transition = "transform 0.8s ease";
+    gameContent.style.transform = "translateX(0)";
+    gameContent.style.transition = "transform 0.8s ease";
+  });
 });
 
 const gameBoard = (() => {
@@ -27,6 +52,8 @@ const gameBoard = (() => {
 })();
 
 const Player = (name, marker) => {
-  let playerName = name;
-  let chosenMarker = marker;
+  const getName = () => name;
+  const getMarker = () => marker;
+
+  return { getName, getMarker };
 };
